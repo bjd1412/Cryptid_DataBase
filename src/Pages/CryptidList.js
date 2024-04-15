caa1f59da70666b8be58a4f3817a65b392cc9f70
Brendan Dempsey <bjd1412@gmail.com>
2024-04-14 21:18:42 -0400

    added more to List function

--- src/Pages/CryptidList.js
+++ src/Pages/CryptidList.js
@@ -1,15 +1,26 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { useOutletContext } from "react-router-dom";
-
+import CryptidCards from "../Components/CryptidCards";
+import Submissions from "../Components/Submissions";
 
 
 function CryptidList () {
    const [crypt, setCrypt] = useOutletContext()
    const [search, setSearch] = useState("")
-   
+
+   function SearchChange (e) {
+      setSearch(e.target.value)
+  }
+
+
    return(
-<h1>placeholder</h1>
+      <div className="Main">
+      <h2 className="List">Cryptid List:</h2>
+      <input className="List"  name="search" placeholder="Search Cryptid..." value={search} onChange={SearchChange}></input>
+      <CryptidCards />
+      <Submissions/>
+      </div>
    )
     
 }
