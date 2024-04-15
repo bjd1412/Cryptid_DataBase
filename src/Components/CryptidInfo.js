14c008ddba74f9fcdb4e5dcd0c0aad2ff3adc82c
Brendan Dempsey <bjd1412@gmail.com>
2024-04-14 21:16:13 -0400

    adding component info

--- src/Components/CryptidInfo.js
+++ src/Components/CryptidInfo.js
@@ -1,15 +1,14 @@
 import { Link } from "react-router-dom"
 
 
-function CryptidInfo () {
-
+function CryptidInfo ({id, name, origin}) {
 
     return(
         <main className="CryptidList">
-            
+            <Link to={`/cryptids/${id}`}>{name}</Link>
+            <span>{origin}</span>
         </main>
     )
 }
-
 
 export default CryptidInfo
