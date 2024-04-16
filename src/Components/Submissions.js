0fb0aa6f12c7a74c42bf328a1c44be94f5feeef5
Brendan Dempsey <bjd1412@gmail.com>
2024-04-16 00:46:22 -0400

    added inputs and values in submissions component

--- src/Components/Submissions.js
+++ src/Components/Submissions.js
@@ -28,8 +28,19 @@ function Submissions ({onHandleSubmit}) {
     }
 
     return(
-        <div className="Main">
-            <h1>Placeholder for Submisson Form</h1>
+        <div className="SubmitForm">
+            <h2>Do you know of a cryptid that isnt on the list? Contribute to the database, and add them to the list.</h2>
+            <form className="NewCryptid" onSubmit={Submitter}>
+                <label  className="Label"> Cryptid Name </label> 
+                <input type="text" name="CryptidName" placeholder="Cryptid Name..." value={name} onChange={(e) => setName(e.target.value)}/>
+                <label  className="Label"> Cryptid Origin </label>
+                <input type="text" name="origin" placeholder="Origin..." value={origin} onChange={(e) => setOrigin(e.target.value)}/>
+                <label  className="Label">Cryptid Lore</label>
+                <input type="text" name="lore" placeholder="Lore..." size="200" className="Big Box" value={lore} onChange={(e) => setLore(e.target.value)}/>
+                <label className="Label">Cryptid Image</label>
+                <input type="text" name="image" placeholder="image url..." value={image} onChange={(e) => setImage(e.target.value)}/>
+                <input type="submit" value="Submit" />  
+            </form>
         </div>
         
     )
