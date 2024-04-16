7b7454273131a8b86e041dbccf86d95fe9e3c658
Brendan Dempsey <bjd1412@gmail.com>
2024-04-16 00:08:37 -0400

    cryptid page fetch setup

--- src/Pages/Cryptids.js
+++ src/Pages/Cryptids.js
@@ -2,6 +2,15 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
 function Cryptids () {
+    const [cryptid, setCryptid] = useState({})
+    const params = useParams()
+    const cryptidId = params.id
+
+    useEffect(() => {
+        fetch(`http://localhost:3000/cryptids/${cryptidId}`)
+        .then(res => res.json())
+        .then(res => setCryptid(res))
+    },[cryptidId])
     
     return (
         <h1>Placeholder</h1>
