82aaea4330b3972806653f5e230bbe2633c96290
Brendan Dempsey <bjd1412@gmail.com>
2024-04-16 00:41:21 -0400

    set up fetch

--- src/Components/Submissions.js
+++ src/Components/Submissions.js
@@ -1,6 +1,31 @@
 import React from "react";
+import { useState } from "react";
 
 function Submissions () {
+    const [name, setName] = useState("")
+    const [origin, setOrigin] = useState("")
+    const [lore, setLore] = useState("")
+    const [image, setImage] = useState("")
+
+    function Submitter (e) {
+        e.preventDefault()
+        const NewForm = {
+            name: name,
+            origin: origin,
+            lore: lore,
+            image: image
+        }
+        fetch("http://localhost:3000/cryptids", {
+            method: "POST",
+            headers:{
+                "Content-Type": "application-json"
+            },
+            body: JSON.stringify(NewForm)
+        })
+        .then(res => res.json())
+        .then(NewCryptid => onHandleSubmit(NewCryptid))
+
+    }
 
     return(
         <div className="Main">
