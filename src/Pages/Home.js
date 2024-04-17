7145661fb8e4e1582eb67ba06d631d8b4a18691b
Brendan Dempsey <bjd1412@gmail.com>
2024-04-16 22:09:20 -0400

    changed homepage

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,6 +4,7 @@ function Home () {
         <div className="Main">
         <h1>Welcome To The Cryptid DataBase</h1>
         <img src="https://assets.apnews.com/68/b4/39953f42b3abefe4b01629385929/10833769a2034dcf9503b083c0967cbc" alt="What are cryptids?" id="Image"/>
+        <h3>Cryptozoology and "Cryptids"</h3>
         <p>Cryptids are creatures</p>
         </div>
     )
