88f66ab9f80df22040ec690c7b672884d030f26a
Brendan Dempsey <bjd1412@gmail.com>
2024-04-16 22:10:41 -0400

    added more to homepage

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,7 +5,9 @@ function Home () {
         <h1>Welcome To The Cryptid DataBase</h1>
         <img src="https://assets.apnews.com/68/b4/39953f42b3abefe4b01629385929/10833769a2034dcf9503b083c0967cbc" alt="What are cryptids?" id="Image"/>
         <h3>Cryptozoology and "Cryptids"</h3>
-        <p>Cryptids are creatures</p>
+        <p>Cryptozoology is a pseudoscience and subculture dedicated to the search for and study of unknown, legendary, or extinct animals whose current existence is questioned or unverified.<br/> 
+        It focuses particularly on creatures popular in folklore, like Bigfoot, the Loch Ness Monster, Yeti, the chupacabra, the Jersey Devil, or the Mokele-mbembe. <br/> Practitioners of cryptozoology term these creatures "cryptids."
+        </p>
         </div>
     )
 }
