47f699f1d1927b9f9a55a82d5cfb3194468a14dc
Brendan Dempsey <bjd1412@gmail.com>
2024-04-16 22:13:18 -0400

    finished homepage

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -8,6 +8,8 @@ function Home () {
         <p>Cryptozoology is a pseudoscience and subculture dedicated to the search for and study of unknown, legendary, or extinct animals whose current existence is questioned or unverified.<br/> 
         It focuses particularly on creatures popular in folklore, like Bigfoot, the Loch Ness Monster, Yeti, the chupacabra, the Jersey Devil, or the Mokele-mbembe. <br/> Practitioners of cryptozoology term these creatures "cryptids."
         </p>
+        <p>Cryptozoology is widely considered a pseudoscience within mainstream scientific circles due to its deviation from the scientific method. Consequently, it often <br/> encounters significant skepticism and is frequently disregarded, not being taken seriously by the scientific community. However, it's worth noting that history has shown instances<br/> where creatures initially dismissed as extinct or mere products of folklore were later proven to be real.<br/>
+        Some of these animals are: Komodo Dragon, giant squid, Mountain Gorilla, Coelacanth, and Okapi.</p>
         </div>
     )
 }
