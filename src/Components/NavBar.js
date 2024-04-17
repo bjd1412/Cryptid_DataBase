43c57ce12bd6a4578c6635d3433ce325b1580566
Brendan Dempsey <bjd1412@gmail.com>
2024-04-16 22:30:27 -0400

    changed Navbar and css

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,7 +2,8 @@ import { NavLink } from "react-router-dom";
 import "../Components/NavBar.css";
 
 function NavBar () {
-    return(<nav>
+    return(
+    <nav className="navbar">
         <NavLink to="/" className="nav-link" > Home</NavLink>
         <NavLink to="/cryptidlist" className="nav-link">Cryptids</NavLink>
     </nav>
